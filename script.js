08e5f52693bf17a5d18d5f5f75a9c9f14146dc4d
Christophe Lionet <tastywithpasta@gmail.com>
2018-03-05 22:56:28 +0100

    Handles button click event using GetElementByID

--- script.js
+++ script.js
@@ -6,30 +6,54 @@
 let suits = [ "Hearts", "Clubs", "Diamonds", "Spades" ]
 let values = [ "Ace", "King", "Queen", "Jack", "Ten", "Nine", 
 "Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
-
+let card = {
+    suit: "Hearts",
+    value: "Queen"
+};
+console.log(card.suit);
+console.log(card.value);
 
 function createDeck(){
-    deck = [];
+    let deck = [];
     for(let i=0; i<suits.length; i++){
         for(let j=0; j<values.length; j++) {
-            deck.push(values[j] + " of " + suits[i]);
+            let card = {
+                suit: suits[i],
+                value: values[j],
+            };
+            deck.push(card);
         }
     }
     return deck;
 }
 
-let deck = createDeck();
-
-function getNextCard(){
-    return deck.shift();
+function getNextCard(deckOfCards){
+    return deckOfCards.shift();
 }
 
+function pickACard(deckOfCards)
+{
+    let spliceIndex = Math.trunc(Math.random() * deckOfCards.length);
+    if(Number.isNaN(spliceIndex))
+        console.log("Woops, there's an error while picking a card!")
+    let cardPicked = deckOfCards.splice(spliceIndex, 1)[0];
+    return cardPicked;
+}
 
+function getCardName(card) {
+    return card.value + " of " + card.suit;
+}
 
-let playerCards = [ deck[0], deck [2] ];
+let deck = createDeck();
+let playerCards = [ pickACard(deck), pickACard(deck) ];
+console.log(getCardName(pickACard(deck)));
+
 
 console.log("Welcome to BlackJack");
-console.log("You are dealt: \n" + playerCards[0] + "\n" + playerCards[1]);
+console.log("You are dealt: \n" + getCardName(playerCards[0]) + "\n" + getCardName(playerCards[1]));
 
 
-console.log(deck);
+let okButton = document.getElementById("ok-button");
+okButton.addEventListener("click", function(){
+    document.getElementById("paragraph").innerText = "Button Clicked!";
+})
