31bf6b0451e8c02800a3cb78d9260d2d19c08d1a
Christophe Lionet <tastywithpasta@gmail.com>
2018-03-05 22:07:47 +0100

    Added deck-generating code.

--- script.js
+++ script.js
@@ -3,8 +3,22 @@
 // by Who Cares
 //
 
-let card1 = "Ace of Spades", 
-    card2 = "Ten of Hearts";
+let suits = [ "Hearts", "Clubs", "Diamonds", "Spades" ]
+let values = [ "Ace", "King", "Queen", "Jack", "Ten", "Nine", 
+"Eight", "Seven", "Six", "Five", "Four", "Three", "Two"]
+let deck = [ ];
+
+for(let i=0; i<suits.length; i++){
+    for(let j=0; j<values.length; j++) {
+        deck.push(values[j] + " of " + suits[i]);
+    }
+}
+
+
+let playerCards = [ deck[0], deck [2] ];
 
 console.log("Welcome to BlackJack");
-console.log("You are dealt: \n" + card1 + "\n" + card2);
+console.log("You are dealt: \n" + playerCards[0] + "\n" + playerCards[1]);
+
+
+console.log(deck);
