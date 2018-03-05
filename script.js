c1b7c668d7501ca513a829e8d4aea3eef723bc48
Christophe Lionet <tastywithpasta@gmail.com>
2018-03-05 21:30:20 +0100

    First (very) basic skeleton for blackjack.

---
+++ script.js
@@ -0,0 +1,10 @@
+//
+// Blackjack
+// by Who Cares
+//
+
+let card1 = "Ace of Spades", 
+    card2 = "Ten of Hearts";
+
+console.log("Welcome to BlackJack");
+console.log("You are dealt: \n" + card1 + "\n" + card2);
